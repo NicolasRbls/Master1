8afd7a1d65ece8d525f8e56b7c27ea1512c9cc92
NicolasRbls <144225477+NicolasRbls@users.noreply.github.com>
2024-09-26 22:59:21 +0200

    modif

---
+++ concept_poo/src/qualite_logicielle/EXO_CH_4/test.java
@@ -0,0 +1,14 @@
+package qualite_logicielle.EXO_CH_4; // Changez ceci par votre propre package
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MaClasseTest {
+
+    @Test
+    public void testFonctionnalite() {
+        // Votre code de test ici
+        assertEquals(4, 2 + 2, "2 + 2 doit faire 4");
+    }
+}
+ 
