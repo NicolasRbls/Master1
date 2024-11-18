53b6669fd23e25f0291697c8c98c5dc1343eabac
NicolasRbls <144225477+NicolasRbls@users.noreply.github.com>
2024-11-18 11:47:55 +0100

    PLATEAU , correction, ameliorations

--- POO_M1/File_Rouge_RPG/game.py
+++ POO_M1/File_Rouge_RPG/game.py
@@ -4,7 +4,10 @@ from race import Race
 from classe import Classe
 from item import Equipment, Bag, Item
 from quest import Quest
-from versus import Versus  # Assurez-vous que la classe Versus est importée correctement
+from versus import Versus  
+from jeu import Jeu
+from plateau import Joueur
+
 
 def main():
     # Configuration des races
@@ -38,6 +41,7 @@ def main():
     # Création d’un autre héros pour le mode Versus
     hero2 = Hero({'name': 'Pierre', 'race': human, 'classe': warrior, 'bag': bagHero2, 'equipment': [], 'element': 'Earth', 'profession': 'Guerrier'})
     
+    """
     # Lancement de la quête
     sword = Equipment({'classList': ['Warrior'], 'place': 'hand', 'name': 'Épée du dragon', 'type': 'sword', 'space': 2}, Stat({'strength': 5, 'magic': 0, 'agility': 5, 'speed': 5, 'charisma': 0, 'chance': 5}))
     
@@ -51,6 +55,20 @@ def main():
         versus.run()  # Lancement du combat Versus
     else:
         print(f"{hero._nom} n'est pas en état de combattre dans le mode Versus.")
+
+    """
+    # Lancement du mode Plateau
+    # Initialisation du jeu
+    jeu = Jeu(size=50, nb_obstacles=10, nb_etapes=20)
+
+    # Ajout des joueurs
+    joueur1 = Joueur(hero)  # Associer Jean au plateau
+    joueur2 = Joueur(hero2)  # Associer Pierre au plateau
+    jeu.ajouter_joueur(joueur1)
+    jeu.ajouter_joueur(joueur2)
+
+    # Lancer le jeu
+    jeu.lancer()
 
 
 if __name__ == "__main__":
